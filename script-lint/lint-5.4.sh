472853bd2fc1ffcd0c30a82c37e4bceef62c1f3c
Nathanael <nathanael.touchard@factorfx.com>
2018-05-16 12:14:14 +0000

    modif build

--- script-lint/lint-5.4.sh
+++ script-lint/lint-5.4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find src/ -name '*.*' -exec /opt/php-5.4.24/sapi/cli/php -l {} \; |grep -v "No syntax errors detected in"
+find -name '*.php' -exec /opt/php-5.4.24/sapi/cli/php -l {} \; |grep -v "No syntax errors detected in"
